fb44381931412e5212379316c78cd35706194912
fnahmed15 <155091437+fnahmed15@users.noreply.github.com>
2024-02-16 00:20:01 +0000

    Delete myOtherScript.js

--- myOtherScript.js
+++
@@ -1,9 +0,0 @@
-var password = "bma123";
-(function passcodeprotect() {
-   var passcode = prompt("Enter PassCode");
-   while (passcode !== password) {
-      alert("Incorrect PassCode");
-      return passcodeprotect();
-   }
-}());
-alert('Welcome to mrahmedcomputing.co.uk');
